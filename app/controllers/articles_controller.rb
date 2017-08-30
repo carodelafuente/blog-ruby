bdef08d564e60dcca3d2ec95efc17378b322c0ce
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-08-30 14:13:45 -0400

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
+  end
+
+
+
+end
