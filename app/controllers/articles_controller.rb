084392a06c31d402c746416165b2ba06cb0da56b
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-08-30 15:44:27 -0400

    added CRUD methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+
   end
 
   def show
@@ -9,6 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
@@ -18,8 +20,37 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :text))
 
+    if @article.save
+        redirect_to @article
+      else
+        render 'new'
+    end
     @article.save
     redirect_to @article
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 
